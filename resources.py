f6a1184d1eee5612ca55710c2bc5175581ed4b22
Mark Aleksander Hil <mark@100shapes.com>
2019-02-18 16:44:37 +0000

    Added request parser

--- resources.py
+++ resources.py
@@ -1,11 +1,18 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument('username', help='This field cannot be blank', required=True)
+parser.add_argument('password', help='This field cannot be blank', required=True)
+
 
 class UserRegistration(Resource):
     def post(self):
+        data = parser.parse_args()
         return {'message': 'User registration'}
 
 class UserLogin(Resource):
     def post(self):
+        data = parser.parse_args()
         return {'message': 'User login'}
 
 class UserLogoutAccess(Resource):
