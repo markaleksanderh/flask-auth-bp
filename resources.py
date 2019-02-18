6d2736b1814c4d6f0ca49282d546cf49254f2f9e
Mark Aleksander Hil <mark@100shapes.com>
2019-02-18 16:40:43 +0000

    Fixed typo

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@ from flask_restful import Resource
 
 class UserRegistration(Resource):
     def post(self):
-        return {'message': 'User User registration'}
+        return {'message': 'User registration'}
 
 class UserLogin(Resource):
     def post(self):
